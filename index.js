0f0ca4c0de4b4056cc7ac74d21efc2febcfb6e21
jonathanx111 <jonathanx111@gmail.com>
2020-12-15 19:03:28 -0500

    final

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+// function superbowlWin(array) {
+//   let result = array.find(function(e){
+//     return e.result === "W"
+//   })
+
+//   return !!result ? result.year : undefined
+// }
+
+superbowlWin = array => {
+  let result = array.find(e => e.result === "W")
+
+  return !!result ? result.year : undefined
+}
